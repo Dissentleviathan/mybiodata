95d5dfa981797607d7c24dbc91a2d6fbf5d04f57
Dissentleviathan <maghasp22@gmail.com>
2023-02-23 11:04:59 +0700

    error

--- app/src/main/java/com/zega/mybiodata/MainActivity.java
+++ app/src/main/java/com/zega/mybiodata/MainActivity.java
@@ -2,13 +2,39 @@ package com.zega.mybiodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Utilities utilities = new Utilities();
+    private TextView tvUsername;
+    private Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Utilities.isLogin(this, "xUsername")) {
+            Intent Intent = new Intent(this, LoginActivity.class);
+            startActivity(Intent);
+            finish();
+        } else {
+            String username = utilities.getPref(this, "xUsername");
+            tvUsername.setText(username);
+        }
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utilities.setPref(MainActivity.this, "xUsername", null);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/zega/mybiodata/Utilities.java
+++ app/src/main/java/com/zega/mybiodata/Utilities.java
@@ -5,7 +5,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 public class Utilities {
-    private SharedPreferences sp;
+    private static SharedPreferences sp;
     private SharedPreferences.Editor editor;
 
     public void setPref(Context context, String key, String value) {
@@ -21,7 +21,7 @@ public class Utilities {
         return value;
     }
 
-    public boolean isLogin(Context context, String key) {
+    public static boolean isLogin(Context context, String key) {
         sp = PreferenceManager.getDefaultSharedPreferences(context);
         String value = sp.getString(key, null);
         return value != null;
